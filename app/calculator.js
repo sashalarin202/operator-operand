1cccfe373b8cd68797a0ddb0accc48074d7b4efa
sashalarin <aleksandr.larin200@gmail.com>
2023-08-20 01:14:19 +0100

    Solution

--- app/calculator.js
+++ app/calculator.js
@@ -1,4 +1,39 @@
 exports.calculate = function(expression) {
-  return 0;
+  let temp = expression.split(' ')
+  let expr = temp.reverse()
+  let stack = []
+
+  for (let i = 0; i < expr.length; i++) {
+      if (
+          expr[i] === '+' ||
+          expr[i] === '-' ||
+          expr[i] === '/' ||
+          expr[i] === '*'
+      ) {
+          let j = stack.pop()
+          let k = stack.pop()
+          let temp = checkOperator(parseInt(j), parseInt(k), expr[i])
+
+          stack.push(temp)
+      } else {
+          stack.push(expr[i])
+      }
+  }
+  return stack
+}
+
+function checkOperator(a, b, token) {
+  switch (token) {
+      case '+':
+          return a + b
+      case '-':
+          return a - b
+      case '/':
+          return a / b
+      case '*':
+          return a * b
+      default:
+          return `Unknown operator: ${token}`
+  }
 }
 
